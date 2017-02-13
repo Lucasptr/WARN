463e30a235f1393018d2b14ca6bd17818d231c4d
Vinícius Campos <vpcampos@outlook.com>
2017-02-13 14:21:09 -0200

    Versionamento do Novo Template

--- 004-Web/JS/Config/routeConfig.js
+++
@@ -1,7 +0,0 @@
-angular.module("WarnClient").config(function($routeProvider) {
-  $routeProvider.when("/login", {
-    templateUrl: "view/login.html",
-    controller: "loginCtrl"
-  });
-  $routeProvider.otherwise({redirectTo : '/login'})
-});
--- 004-Web/JS/Controllers/loginController.js
+++
@@ -1,3 +0,0 @@
-angular.module("WarnClient").controller("loginCtrl", function ($scope) {
-
-});
